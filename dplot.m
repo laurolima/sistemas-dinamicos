2ab41e3e3842e875d883b021b0478b30b83295db
Lauro de Lima <laurolfh@gmail.com>
2016-06-04 16:14:23 -0300

    time series added

--- dplot.m
+++ dplot.m
@@ -3,10 +3,12 @@ function ho = dplot (vetor, sp, tipo, parg)
   
   switch tipo
     case "rmapv"
-      ho = plot([vetor(end-1),vetor(end-1)],[vetor(end-1),vetor(end)],parg)
+      ho = plot([vetor(end-1),vetor(end-1)],[vetor(end-1),vetor(end)],parg);
     case "rmaph"
-      ho = plot([vetor(end-1),vetor(end)],[vetor(end),vetor(end)],parg)
+      ho = plot([vetor(end-1),vetor(end)],[vetor(end),vetor(end)],parg);
     case "xyplot"
-      ho = plot(vetor(end-1),vetor(end),parg)
+      ho = plot(vetor(end-1),vetor(end),parg);
+    case "tseries"
+      ho = plot([length(vetor)-1,length(vetor)],[vetor(end-1),vetor(end)]);
   end
 end
--- iterate.m
+++ iterate.m
@@ -2,32 +2,43 @@ clc; clear;
 
 erase = 1;
 x = rand(1);
+r = rand(1)
 
 h1 = figure;
 hold all;
 
-s1 = subplot(1,2,1)
+s1 = subplot(2,2,1)
 title("f(f(x)),f(x)")
-plot([0:0.01:1],tent([0:0.01:1]))
+plot([0:0.01:1],tent([0:0.01:1])) %tentmap
 axis square;
 hold on;
-plot([0,1],[0,1],'Color',[0,0.4,0.2])
+plot([0,1],[0,1],'Color',[0,0.4,0.2]) %y=x
 
 
-s2 = subplot(1,2,2)
+s2 = subplot(2,2,2)
 title("x,f(x)");
+%xlim([0,1])
+ylim([0,1])
+axis square;
+hold on;
+
+s4 = subplot(2,2,4)
+title("random");
 plot(x(1:end-1),x(2:end),'+')
-xlim([0,1])
+%xlim([0,1])
 ylim([0,1])
 axis square;
 hold on;
 
 k=0;
-for i = 1:100
+i=1;
+while true
   if mod(i,2)
-    x = [x, tent(x(end))];
-    hxy = dplot(x,s2,"xyplot",'mo');
+    x   = [x, tent(x(end))];
+    r   = [r, rand(1)];
+    hxy = dplot(x,s2,"tseries",'k');
     hmh = dplot(x,s1,"rmapv","k--o");
+    hrg = dplot(r,s4,"tseries",'k');
   else
     hmv = dplot(x,s1,"rmaph","k--o");
   end
@@ -35,6 +46,7 @@ for i = 1:100
   if k == 0
     k = waitforbuttonpress;
   endif
+  i = i+1
 end
 %
 %oa1 = 0;
